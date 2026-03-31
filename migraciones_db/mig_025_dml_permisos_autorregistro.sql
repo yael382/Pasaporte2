67653216cd78f3cb0a6ff8b195cc2a884f6d31bc
Rubén Ramírez <maths.unam@gmail.com>
2026-03-30 21:58:49 -0600

    Code updates

--- migraciones_db/mig_025_dml_permisos_autorregistro.sql
+++ migraciones_db/mig_025_dml_permisos_autorregistro.sql
@@ -1,7 +1,7 @@
 INSERT IGNORE INTO
     `permiso` (`tipo`, `codename`, `nombre`)
 VALUES (
-        'usuario',
+        'otro',
         'autorregistrarse',
         'Registrarse a sí mismo en eventos (Autorregistro)'
     );
