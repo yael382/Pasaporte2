43555fb38e92d5b7e0a6afb8fa5b0f0f268312c1
Rubén Ramírez <maths.unam@gmail.com>
2026-03-31 00:57:23 -0600

    CodeUpdates

--- app/usuario/autoregistro.php
+++ app/usuario/autoregistro.php
@@ -10,16 +10,34 @@
                 <th>Nombre</th>
                 <th>Fecha</th>
                 <th>Lugar</th>
-                <th></th>
+                <!--<th></th>-->
             </tr>
         </thead>
         <tbody>
             <?php foreach ($eventos as $ev): ?>
                 <tr>
-                    <td><?php echo htmlspecialchars($ev['nombre']); ?></td>
-                    <td><?php echo htmlspecialchars($ev['fecha_hora']); ?></td>
-                    <td><?php echo htmlspecialchars($ev['lugar']); ?></td>
-                    <td>
+                    <td style="border: none;">
+                        <?php echo htmlspecialchars($ev['nombre']); ?>
+                    </td>
+                    <td style="border: none;">
+                        <?php echo htmlspecialchars($ev['fecha_hora']); ?>
+                    </td>
+                    <td style="border: none;">
+                        <?php echo htmlspecialchars($ev['lugar']); ?>
+                    </td>
+                    <!--<td>
+                        <form method="post" style="margin:0; display:flex; gap: 0.5rem; align-items: center;">
+                            <input type="hidden" name="evento_id" value="<?php echo htmlspecialchars($ev['id']); ?>">
+                            <input type="text" name="equipo" class="form-control form-control-sm" placeholder="Nombre de equipo" style="width: 150px;">
+                            <button title="Registrarme" type="submit" class="btn btn-primary btn-sm text-nowrap">
+                                <i class="fa-solid fa-right-to-bracket"></i>
+                                Registrarme
+                            </button>
+                        </form>
+                    </td>-->
+                </tr>
+                <tr>
+                    <td colspan="3">
                         <form method="post" style="margin:0; display:flex; gap: 0.5rem; align-items: center;">
                             <input type="hidden" name="evento_id" value="<?php echo htmlspecialchars($ev['id']); ?>">
                             <input type="text" name="equipo" class="form-control form-control-sm" placeholder="Nombre de equipo" style="width: 150px;">
